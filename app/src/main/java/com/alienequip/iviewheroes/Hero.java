66e3a9ca39230ad6e90f1b626dbb4bf2043ca8a2
rizkireynaldi <georizki@gmail.com>
2019-07-14 08:35:41 +0700

    create mode list

---
+++ app/src/main/java/com/alienequip/iviewheroes/Hero.java
@@ -0,0 +1,31 @@
+package com.alienequip.iviewheroes;
+
+class Hero {
+    private String name;
+    private String from;
+    private String photo;
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getFrom() {
+        return from;
+    }
+
+    void setFrom(String from) {
+        this.from = from;
+    }
+
+    String getPhoto() {
+        return photo;
+    }
+
+    void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
